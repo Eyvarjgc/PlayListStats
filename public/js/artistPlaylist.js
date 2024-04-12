93c6d2b0b125af24e7aa646c91f6e42eec4b52e3
Eyvar <eyvar31@gmail.com>
2024-04-12 12:01:17 -0500

    Feat: Create playlist, cover image and add tracks addes

--- public/js/artistPlaylist.js
+++ public/js/artistPlaylist.js
@@ -1,10 +1,15 @@
 import {accessToken} from './saved-token.js'
+import {imgFile} from './defaultImageEncoded.js'
 
 
 const form = document.querySelector('.form')
 const playInput = document.querySelector('.playInput')
 const submit = document.querySelector('.submit')
 const playlists = document.querySelector('.playlists')
+const artistsInput = document.querySelector('.artistsInput')
+const artistForm = document.querySelector('.artistForm')
+const playlistName = document.querySelector('.playlistName')
+
 
 
 form.addEventListener('submit', async (e) => {
@@ -25,19 +30,37 @@ form.addEventListener('submit', async (e) => {
     const getArtistsInPL = dataInThePlaylist.map((element) => {
       return element.track.artists[0].name
     })
-
-    const artistsInput = 'Calmadò'
-
-    const getMusic = dataInThePlaylist.filter(element => {
-      return element.track.artists[0].name == artistsInput
-    })
-
     console.log('Some artists in this pl');
     console.log(getArtistsInPL);
 
+    artistForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      if(artistsInput.value){
+        const artistsInputValue = artistsInput.value
+  
+        const getMusic = dataInThePlaylist.filter(element => {
+          return element.track.artists[0].name == artistsInputValue
+        })
+    
+        const tracksUris = getMusic.map(element => {
+          return element.track.uri
+        })
+        
+        console.log(tracksUris);
+        
+  
+        console.log('Artist`s music ');
+        console.log(getMusic);
+        createPlaylist(accessToken, tracksUris, playlistName.value)
+        // addTracksToPlaylist(accessToken, tracksUris, )
+        // playlistID
+    
+
+      }
+
+    })
 
 
-// <3
   }
 })
 
@@ -59,11 +82,12 @@ async function getPlaylistItems(InsertAccessToken, playlistInfo ){
   }
 }
 
-async function createPlaylist(InsertAccessToken){
+async function createPlaylist(InsertAccessToken , uris, playlistName){
   try{
-    const data = await axios.post('https://api.spotify.com/v1/users/2126enqo2356up46fiz7nx7iq/playlists', 
+    const clientID = ''
+    const data = await axios.post(`https://api.spotify.com/v1/users/2126enqo2356up46fiz7nx7iq/playlists`, 
     {
-      "name": "Test Playlist",
+      "name": `${playlistName}`,
       "description": "New playlist description",
       "public": false
   }, {
@@ -73,15 +97,64 @@ async function createPlaylist(InsertAccessToken){
     }
 })
 
-  console.log(data);
+  updateCoverImgPlaylist(InsertAccessToken, data.data.id)
+  addTracksToPlaylist(InsertAccessToken, uris, data.data.id)
+  
+  }catch(e){
+    console.log(e);
+  }
+}
+
+
+async function addTracksToPlaylist(InsertAccessToken, uris, playlistID){
+  try{
+    const tracksToAdd = uris
+    const response = await axios.post(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, 
+    {
+      "uris": tracksToAdd
+        
+        
+    
+    }, {
+      headers:{
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer' + InsertAccessToken
+      }
+    })
+
+    console.log(response);
 
 
 
   }catch(e){
     console.log(e);
   }
+
 }
-// createPlaylist(accessToken)
+
+
+
+
+async function updateCoverImgPlaylist(InsertAccessToken, playlistId ){
+  try{
+  
+    const response = await axios.put(`https://api.spotify.com/v1/playlists/${playlistId}/images`, imgFile
+      , {
+      headers:{
+        'Content-Type': 'image/jpeg',
+        Authorization: 'Bearer' + InsertAccessToken
+            }
+    })
+
+    console.log(response);
+
+
+
+  }catch(e){
+    console.log(e);
+  }
+
+}
 
 
 // Get user playlist and insert in the html
--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/login', function(req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
   // your application requests authorization
-  var scope = 'user-read-private user-read-email playlist-read-private user-top-read      user-library-read playlist-modify-private';
+  var scope = 'user-read-private user-read-email playlist-read-private user-top-read ugc-image-upload  user-library-read playlist-modify-private';
   res.redirect('https://accounts.spotify.com/authorize?' +
     stringify({
       response_type: 'code',
